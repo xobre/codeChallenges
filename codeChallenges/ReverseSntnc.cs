289316bd5c1058d37bd23d46b874188bb3fb7951
xobre <mua.breana@gmail.com>
2019-10-22 15:33:15 -0700

    Solved W3 Reverse Sentence

---
+++ codeChallenges/ReverseSntnc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    static class ReverseSntnc
+    {
+        static void reverseTheSntnc()
+        {
+            Console.Write("USER INPUT: ");
+            string origSntnc = Console.ReadLine();
+            string[] scndSntnc = origSntnc.Split(' ');
+            Console.WriteLine(origSntnc);
+            Array.Reverse(scndSntnc);
+            Console.WriteLine(String.Join(" ", scndSntnc));
+        }
+    }
+}
