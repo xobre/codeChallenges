839fc2c67513345f16fc841f4ccc4fa3f4a15e71
xobre <mua.breana@gmail.com>
2019-10-23 13:09:45 -0700

    Program checks how many times a certain value is in a randomized array.

---
+++ codeChallenges/HowManyInArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace codeChallenges
+{
+    class HowManyInArray
+    {
+        public static void Count()
+        {
+            int Min = 0;
+            int Max = 6;
+            Random randNum = new Random();
+            int[] test2 = Enumerable
+                .Repeat(0, 20)
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            Console.WriteLine(String.Join(" ", test2));
+            int x = 3;
+            int count = 0;
+            foreach (int i in test2)
+            {
+                if (i.Equals(x))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"{x} was in the array {count} time(s)");
+        }
+    }
+}
