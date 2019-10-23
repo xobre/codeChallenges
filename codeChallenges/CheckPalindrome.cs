713349922d780a520545b3b22a26f50a486ada11
xobre <mua.breana@gmail.com>
2019-10-23 14:08:34 -0700

    Palindrome checker

---
+++ codeChallenges/CheckPalindrome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class CheckPalindrome
+    {
+        public static void Palindrome()
+        {
+            Console.WriteLine("User Input: ");
+            string pali = Console.ReadLine();
+            char[] c = pali.ToCharArray();
+            Array.Reverse(c);
+            Console.WriteLine(new string(c).Equals(pali));
+        }
+    }
+}
