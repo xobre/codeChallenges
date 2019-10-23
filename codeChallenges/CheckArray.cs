ed1ce0773d91b07c77ef6d11e0846fb70e624f85
xobre <mua.breana@gmail.com>
2019-10-23 13:13:38 -0700

    Checks if there is an odd number in given array.

---
+++ codeChallenges/CheckArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class CheckArray
+    {
+        public static void IfOdd()
+        {
+            int[] origArray = { 2, 4, 7, 8, 6 };
+            int count = 0;
+            for (int i = 0; i < origArray.Length; i++)
+            {
+                if (origArray[i] % 2 != 0)
+                {
+                    count++;
+                }
+            }
+            if (count >= 1)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
