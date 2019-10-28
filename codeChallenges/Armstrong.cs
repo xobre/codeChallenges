f0b5e730d0251ae01142a2f073e7a71a0d4e5323
xobre <mua.breana@gmail.com>
2019-10-28 09:54:16 -0700

    Program checks if number is Armstrong #.

---
+++ codeChallenges/Armstrong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class Armstrong
+    {
+        public static bool IsArmstrongNumber(int number)
+        {
+            List<int> remainders = new List<int>();
+            int sum = 0;
+            int count = 0;
+            int n = number;
+
+            while (n > 0)
+            {
+                remainders.Add(n % 10);
+                n /= 10;
+                count++;
+            }
+
+            foreach (var el in remainders)
+            {
+                sum += (int)Math.Pow(el, count);
+            }
+
+            if (sum == number)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
