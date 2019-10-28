3a63896ed2078356cdc31ec471904d95850ade07
xobre <mua.breana@gmail.com>
2019-10-28 11:02:58 -0700

    Checks # of differences between two strings.

---
+++ codeChallenges/Hamming.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class Hamming
+    {
+        public static int Distance(string firstStrand, string secondStrand)
+        {
+            int difference = 0;
+            if (firstStrand.Length != secondStrand.Length)
+            {
+                throw new ArgumentException();
+            }
+            for (int i = 0; i < firstStrand.Length; i++)
+            {
+                if (firstStrand[i] != secondStrand[i])
+                {
+                    difference++;
+                }
+            }
+            return difference;
+        }
+    }
+}
