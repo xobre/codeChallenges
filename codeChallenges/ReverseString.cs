7bdd64957b6c2776388c42da085292e425e38668
xobre <mua.breana@gmail.com>
2019-10-28 14:59:38 -0700

    Reversean input string

---
+++ codeChallenges/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class ReverseString
+    {
+        public static string Reverse(string input)
+        {        
+            if (input == null) return null;
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string (charArray);
+        }
+    }
+}
