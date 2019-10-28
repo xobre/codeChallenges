69dfb18b064ff83151779c36281827b214e5247d
xobre <mua.breana@gmail.com>
2019-10-28 15:43:21 -0700

    Extract multiple strings

---
+++ codeChallenges/Series.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class Series
+    {
+        public static string[] Slices(string numbers, int sliceLength)
+        {
+            List<string> slices = new List<string>();
+            if (sliceLength > numbers.Length || sliceLength == null || numbers == null || sliceLength <= 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                for (int i = 0; i < (numbers.Length - (sliceLength - 1)); i++)
+                {
+                    slices.Add(numbers.Substring(i, sliceLength));
+                }
+            }
+            string[] array = slices.ToArray();
+
+            return array;
+        }
+    }
+}
