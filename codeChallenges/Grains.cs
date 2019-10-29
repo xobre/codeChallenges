fd812a6984faf54c2d4815a9956ad08f3e580748
xobre <mua.breana@gmail.com>
2019-10-29 13:54:38 -0700

    Checkerboard counter

---
+++ codeChallenges/Grains.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    public static class Grains
+    {
+        public static ulong Square(int n)
+        {
+            ulong sum = 0;
+            ulong count = 0;
+
+            if (n <= 0 || n > 64)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            else
+            {
+                count = Convert.ToUInt64(n);
+                for (ulong i = 1; i <= count; i++)
+                {
+                    sum += i;
+                    --count;
+                    while (count != 0)
+                    {
+                        sum = sum * 2;
+                        --count;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public static ulong Total()
+        {
+            ulong newSum = 0;
+            for (int i = 1; i <= 64; i ++)
+            {
+                newSum += Square(i) + Square(++i);
+            }
+            return newSum;
+        }
+    }
+}
+
