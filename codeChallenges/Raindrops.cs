1347285d857384174410f06088a5a64ecf6beafd
xobre <mua.breana@gmail.com>
2019-10-30 11:11:28 -0700

    Raindrops *similar to FizzBuzz

---
+++ codeChallenges/Raindrops.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class Raindrops
+    {
+        public static string Convert(int number)
+        {
+            string String = "";
+            bool Factor = false;
+            if (number % 3 == 0)
+            {
+                Factor = true;
+                String += "Pling";
+            }
+            if (number % 5 == 0)
+            {
+                Factor = true;
+                String += "Plang";
+            }
+            if (number % 7 == 0)
+            {
+                Factor = true;
+                String += "Plong";
+            }
+            if (Factor == false) return number.ToString();
+            
+            return String;
+        }
+    }
+}
