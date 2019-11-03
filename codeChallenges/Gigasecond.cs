4d3f22a99583fbbc73410aae65772643cd9bb440
xobre <mua.breana@gmail.com>
2019-11-03 11:37:21 -0800

    Add a Gigasecond to a moment

---
+++ codeChallenges/Gigasecond.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class Gigasecond
+    {
+        public static DateTime Add(DateTime moment)
+        {
+            return moment.AddSeconds(1000000000);
+        }
+    }
+}
