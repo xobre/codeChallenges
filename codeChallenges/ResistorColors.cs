0617e5ddde4683315c0434c06d01d358cc1702ae
xobre <mua.breana@gmail.com>
2019-11-03 11:38:42 -0800

    Enum &Array practice

---
+++ codeChallenges/ResistorColors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class ResistorColors
+    {
+        enum ColorEnum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
+        public static int ColorCode(string color)
+        {
+            return (int)Enum.Parse(typeof(ColorEnum), color, true);
+        }
+
+        public static string[] Colors()
+        {
+            string[] ColorArray = Enum.GetNames(typeof(ColorEnum));
+            return ColorArray;
+        }
+    }
+}
