a20ea75a9a7de7710c2a8e109c5ffa2685b8dbbb
xobre <mua.breana@gmail.com>
2019-11-03 11:39:26 -0800

    Practice using double

---
+++ codeChallenges/SpaceAge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    public class SpaceAge
+    {
+        double seconds;
+        public SpaceAge(int seconds) => this.seconds = seconds;
+
+        public double OnEarth() => seconds / 31_557_600;
+
+        public double OnMercury() => this.OnEarth() / 0.2408467;
+
+        public double OnVenus() => this.OnEarth() / 0.61519726;
+
+        public double OnMars() => this.OnEarth() / 1.8808158;
+
+        public double OnJupiter() => this.OnEarth() / 11.862615;
+
+        public double OnSaturn() => this.OnEarth() / 29.447498;
+
+        public double OnUranus() => this.OnEarth() / 84.016846;
+
+        public double OnNeptune() => this.OnEarth() / 164.79132;
+    }
+}
