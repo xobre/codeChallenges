4ec39ced1a137f19bbe5017884f5718d9549b97d
xobre <mua.breana@gmail.com>
2019-11-15 18:21:36 -0800

    Recursion

---
+++ codeChallenges/Collatz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    public static class CollatzConjecture
+    {
+        public static int Steps(int number, int count = 0)
+        {
+            if (number < 1)
+            {
+                throw new ArgumentException();
+            }
+            if (number == 1)
+            {
+                return (count);
+            }
+            else if (number % 2 == 0)
+            {
+                number = number / 2;
+                count++;
+                Steps(number, count);
+            }
+            else
+            {
+                number = number * 3 + 1;
+                count++;
+                Steps(number, count);
+            }
+            return count;
+        }
+    }
+}
