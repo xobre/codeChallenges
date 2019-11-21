829ccbeb251f6f921998169a6090285600fc96fa
xobre <mua.breana@gmail.com>
2019-11-20 18:31:58 -0800

    Bool &  Hash Set

---
+++ codeChallenges/Pangram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace codeChallenges
+{
+    class Pangram
+    {
+        public static bool IsPangram(string input)
+        {
+            if (input == "") return false;
+            input = input.ToLower();
+            Regex rgx = new Regex("[^a-z]");
+            input = rgx.Replace(input, "");
+            HashSet<char> hash = new HashSet<char>(input);
+            if (hash.Count < 26) return false;
+            else return true;
+        }
+    }
+}
