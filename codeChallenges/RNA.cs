f3f1146a3e9d32154d9e8b05ce5daecefc190047
xobre <mua.breana@gmail.com>
2019-11-20 18:33:30 -0800

    For loop with if & else if

---
+++ codeChallenges/RNA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeChallenges
+{
+    class RNA
+    {
+        public static string ToRna(string nucleotide)
+        {
+            string result = "";
+            if (nucleotide == "") return result;
+            for (int i = 0; i < nucleotide.Length; i++)
+            {
+                if (nucleotide[i] == 'C') result += "G";
+                else if (nucleotide[i] == 'G') result += "C";
+                else if (nucleotide[i] == 'T') result += "A";
+                else if (nucleotide[i] == 'A') result += "U";
+            }
+            return result;
+        }
+    }
+}
